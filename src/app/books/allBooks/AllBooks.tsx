b759052bdb9b029582a3f74013e3d5b876591308
KhEliash <eliashebrahim@gmail.com>
2025-08-02 21:16:33 +0600

    all books route and table

---
+++ src/app/books/allBooks/AllBooks.tsx
@@ -0,0 +1,50 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useGetBooksQuery } from "@/redux/api/booksApi";
+
+const AllBooks = () => {
+  const { data: books, isLoading } = useGetBooksQuery(undefined);
+
+  if (isLoading) return <p className="text-center py-6">Loading...</p>;
+
+  return (
+    <Card className="p-4">
+      <h2 className="text-2xl font-bold mb-4">📚 All Books</h2>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Author</TableHead>
+              <TableHead>Genre</TableHead>
+              <TableHead>ISBN</TableHead>
+              <TableHead>Copies</TableHead>
+              <TableHead>Available</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {books?.data?.map((book: any) => (
+              <TableRow key={book._id}>
+                <TableCell>{book.title}</TableCell>
+                <TableCell>{book.author}</TableCell>
+                <TableCell>{book.genre}</TableCell>
+                <TableCell>{book.isbn}</TableCell>
+                <TableCell>{book.copies}</TableCell>
+                <TableCell>{book.available ? "✅" : "❌"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AllBooks;
--- src/route.tsx
+++ src/route.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import CreateBook from "./app/books/create/CreateBook";
 import Home from "./app/home/Home";
+import AllBooks from "./app/books/allBooks/AllBooks";
 
 export const router = createBrowserRouter([
   {
@@ -10,11 +11,15 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home/>,
+        element: <Home />,
       },
       {
         path: "/create-book",
         element: <CreateBook />,
+      },
+      {
+        path: "/books",
+        element: <AllBooks />,
       },
     ],
   },
