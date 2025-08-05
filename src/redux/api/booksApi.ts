4223f7a2d6ff03e37a9e3a3f5194cfdf5f9e66ce
KhEliash <eliashebrahim@gmail.com>
2025-08-06 00:18:39 +0600

    deploy

--- src/redux/api/booksApi.ts
+++ src/redux/api/booksApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const booksApi = createApi({
   reducerPath: "booksApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://library-backend-lime.vercel.app/" }),
   tagTypes: ["Book"],
   endpoints: (build) => ({
     getBooks: build.query({
